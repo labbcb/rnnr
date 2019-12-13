bd6a4d25e5bcdae30f43a54d9ce1bc1c8380237b
Welliton Souza <well309@gmail.com>
2019-12-12 21:52:16 -0300

    Improve tasks cmd

--- cmd/tasks.go
+++ cmd/tasks.go
@@ -5,7 +5,6 @@ import (
 	"github.com/labbcb/rnnr/client"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	"time"
 )
 
 var tasksCmd = &cobra.Command{
@@ -16,37 +15,15 @@ var tasksCmd = &cobra.Command{
 		host := viper.GetString("host")
 		resp, err := client.ListTasks(host)
 		fatalOnErr(err)
-		fmt.Printf("%-36s   %-19s   %-19s   %-19s   %-18s   %-14s   %s\n",
-			"Task ID", "Created", "Started", "Completed", "Resources", "State", "Name")
-
-		var start, end string
+		fmt.Printf("%-36s  %-18s   %-14s   %s\n", "Task ID", "Resources", "State", "Name (Server)")
 
 		for _, t := range resp.Tasks {
-			if t.Logs != nil {
-				start = formatDateTime(t.Logs.StartTime)
-				end = formatDateTime(t.Logs.EndTime)
-			} else {
-				start = ""
-				end = ""
-			}
-
-			fmt.Printf("%36s | %-19s | %-19s | %-19s | CPU=%02d RAM=%05.2fGB | %-14s | %s\n",
-				t.ID,
-				formatDateTime(t.CreationTime),
-				start,
-				end,
-				t.Resources.CPUCores,
-				t.Resources.RAMGb,
-				t.State,
-				t.Name)
+			fmt.Printf("%36s | CPU=%02d RAM=%05.2fGB | %-14s | %s (%s)\n",
+				t.ID, t.Resources.CPUCores, t.Resources.RAMGb, t.State, t.Name, t.RemoteHost)
 		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(tasksCmd)
-}
-
-func formatDateTime(time time.Time) string {
-	return time.Format("2006-01-02 15:04:05")
 }
