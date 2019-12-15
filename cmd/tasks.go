455835a7a5b31615f1a6405e477708ebeda1849f
Welliton Souza <well309@gmail.com>
2019-12-15 13:57:51 -0300

    Improve tasks cmd output

--- cmd/tasks.go
+++ cmd/tasks.go
@@ -17,9 +17,15 @@ var tasksCmd = &cobra.Command{
 		fatalOnErr(err)
 		fmt.Printf("%-36s   %-18s   %-14s   %s\n", "Task ID", "Resources", "State", "Name")
 
+		var name string
 		for _, t := range resp.Tasks {
-			fmt.Printf("%36s | CPU=%02d RAM=%05.2fGB | %-14s | %s at %s\n",
-				t.ID, t.Resources.CPUCores, t.Resources.RAMGb, t.State, t.Name, t.RemoteHost)
+			if t.RemoteHost != "" {
+				name = t.Name + " at " + t.RemoteHost
+			} else {
+				name = t.Name
+			}
+			fmt.Printf("%36s | CPU=%02d RAM=%05.2fGB | %-14s | %s\n",
+				t.ID, t.Resources.CPUCores, t.Resources.RAMGb, t.State, name)
 		}
 	},
 }
