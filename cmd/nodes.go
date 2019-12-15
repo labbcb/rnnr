2adbfabc4c96acc52278416c40bf017c9261fbc1
Welliton Souza <well309@gmail.com>
2019-12-15 13:49:08 -0300

    Improve nodes cmd output

--- cmd/nodes.go
+++ cmd/nodes.go
@@ -14,8 +14,17 @@ var nodesCmd = &cobra.Command{
 		host := viper.GetString("host")
 		ns, err := client.ListNodes(host)
 		fatalOnErr(err)
+
+		var status string
+		fmt.Printf("%-36s   %-29s   %s   %-8s   %s\n", "ID", "Resources", "Tasks", "Status", "URL")
 		for _, n := range ns {
-			fmt.Println(n)
+			if n.Active {
+				status = "ACTIVE"
+			} else {
+				status = "INACTIVE"
+			}
+			fmt.Printf("%-36s | CPU=%02d/%02d RAM=%06.2f/%06.2fGB | %02d    | %-8s | %s\n",
+				n.ID, n.Usage.CPUCores, n.Info.CPUCores, n.Usage.RAMGb, n.Info.RAMGb, n.Usage.Tasks, status, n.Host)
 		}
 	},
 }
